56e45e8e258954b8d0390b02651ad6866bd10f48
Envy <vungnguyenn1001@gmail.com>
2025-07-30 15:56:40 +0700

    update version build

--- js/firebase-counter.js
+++ js/firebase-counter.js
@@ -21,6 +21,7 @@ const db = getDatabase(app);
 
 const file_links = [
     { version: "Avatar259", url: "https://github.com/envyH/avatar-site/releases/download/v2.5.9/Avatar259.jar" },
+    { version: "Avatar259x2", url: "https://github.com/envyH/avatar-site/releases/download/v2.5.9/Avatar259x2.jar" },
     { version: "Avatar259x3", url: "https://github.com/envyH/avatar-site/releases/download/v2.5.9/Avatar259x3.jar" },
 ];
 
